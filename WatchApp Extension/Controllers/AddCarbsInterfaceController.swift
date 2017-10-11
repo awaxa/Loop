c168b71a88a0fdef7e204856bb20d31ff742ec8f
Greg Kitson <greg@gregkitson.com>
2017-10-11 15:35:30 -0700

    personalize upstream

--- WatchApp Extension/Controllers/AddCarbsInterfaceController.swift
+++ WatchApp Extension/Controllers/AddCarbsInterfaceController.swift
@@ -19,8 +19,8 @@ final class AddCarbsInterfaceController: WKInterfaceController, IdentifiableClas
                 return
             }
 
-            guard carbValue <= 100 else {
-                carbValue = 100
+            guard carbValue <= 250 else {
+                carbValue = 250
                 return
             }
 
@@ -127,7 +127,7 @@ final class AddCarbsInterfaceController: WKInterfaceController, IdentifiableClas
     fileprivate var accumulatedRotation: Double = 0
 }
 
-fileprivate let rotationsPerCarb: Double = 1/24
+fileprivate let rotationsPerCarb: Double = 1/60
 
 extension AddCarbsInterfaceController: WKCrownDelegate {
     func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
